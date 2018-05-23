8f2a9bafc3d8412600cda92e4c0bbd1d30f6a676
chelsyehannum <chelsye.hannum@gmail.com>
2018-05-22 22:46:04 -0400

    HannumJavaScriptExercises

---
+++ script.js
@@ -0,0 +1,63 @@
+//*****EXERCISE 1***** 
+
+//var name = prompt("Enter a number!");
+
+
+//var total = 0;
+//for (var i = 1; i <= name; i++) {
+//total = total + i;
+//}
+//console.log(total);
+
+
+
+//******EXERCISE 2  
+
+//var question = prompt("Do you want to play?");
+//var answer = prompt("Enter a word.");
+
+//do {
+// document.write(answer)
+//  prompt("Do you want to play again?");
+//}
+//while (answer == yes);
+
+
+
+/////*******EXERCISE 3 */
+
+//var name = prompt("Would you like to print your name?");
+
+//var i = "!";
+//while (name === "yes") {
+//  document.write("Hello. My name is Adam" + i);
+//i++;
+//name = prompt("Would you like to print again?");
+//}
+
+
+///***** EXERCISE 4*/
+
+//var timeOfDay = prompt("What time of day is it? Morning, noon, or night?");
+
+//var mealType = 'breakfast';
+//var mealType = 'lunch';
+//var mealType = 'dinner';
+
+//var food = 'eggs and toast';
+//var food = 'soup and salad';
+//var food = 'seafood and steak';
+
+//if (timeOfDay == 'morning') {
+//mealType = 'breakfast';
+//food = 'eggs and toast';
+//} 
+//else if (timeOfDay == 'noon') {
+//  mealType = 'lunch';
+// food = 'soup and salad';
+//} 
+//else if (timeOfDay = 'evening') {
+//  mealType = 'dinner';
+//food = 'seafood and steak';
+//}
+//console.log('Since it is ' + timeOfDay + ' you should eat ' + food + ' for ' + mealType);
